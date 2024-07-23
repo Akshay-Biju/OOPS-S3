b7cbdcf0689904f53a42e6bc60649c438dfadd47
Akshay-Biju <akshaytb2005@gmail.com>
2024-07-24 02:42:51 +0530

    Sum.java

--- Sum.java
+++ Sum.java
@@ -4,14 +4,14 @@ public class Sum
 	public static void main(String args[])
         {
 
-            Scanner S1= new Scanner(System.in);
+            Scanner sc= new Scanner(System.in);
             System.out.println("Enter Two Numbers:");
-            float a=S1.nextFloat();
-            float b=S1.nextFloat(); 
-            float sum=a+b;
+            int a=sc.nextInt();
+            float b=sc.nextFloat(); 
+            int sum=a+b;
             float avg = sum/2;
             System.out.println ("The sum of is:" +sum);
             System.out.println("The average of the numbers is "+avg);
-            S1.close();
+            sc.close();
 		}
 }
